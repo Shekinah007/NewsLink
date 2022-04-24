dd2fd8b8958e022909269df94d8f0f2cf3fd5e04
Shekinah <shalomsheks@gmail.com>
2022-04-24 17:01:51 +0100

    # Search feature (Using the prompt() method for now. ++) # Sort by categories feature ++ # Styling...

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,23 +4,29 @@ import NewsBody from "./Components/NewsBody.js";
 import CategoriesNavBar from "./Components/CategoriesNavBar.js";
 
 export default function App() {
-  console.clear();
-  // console.log(document.getElementById("general").innerText);
+  // console.clear();
+  let date = new Date();
+  let today =
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
   // let section = document.getElementById("general");
   // let names = section.id;
-  // console.log(names);
-  let [q, setQ] = React.useState("russia");
+  // console.log("NAMES ID: ", names);
+  ///////////////////////////////////////////////////////////////////
+  let [q, setQ] = React.useState("cats");
   function search() {
     setQ(prompt("Please enter your query"));
+    setNewsToggle(true);
   }
   let [newsData, setNewsData] = React.useState([]);
   React.useEffect(
     function () {
-      console.log("Use Effect");
       fetch(
         "https://newsapi.org/v2/everything?q=" +
           q +
-          "&from=2022-03-23&sortBy=publishedAt&language=en&apiKey=bd662b9c617d48bca5aa4e291b6ea52f"
+          "&from=" +
+          today +
+          "&sortBy=relevancy&language=en&apiKey=bd662b9c617d48bca5aa4e291b6ea52f"
       )
         .then((res) => res.json())
         .then((answer) => setNewsData(answer.articles));
@@ -28,16 +34,21 @@ export default function App() {
     [q]
   );
 
-  let [newsByCategory, setNewsByCategory] = React.useState();
+  let [newsByCategory, setNewsByCategory] = React.useState([]);
   let [currentCategory, setCurrentCategory] = React.useState("general");
-  function changeCategory() {
-    setCurrentCategory(document.getElementById("general").innerHTML);
+  function changeCategory(event) {
+    console.log("EVENT ID: ", event.target.id);
+
+    setCurrentCategory(event.target.id);
+    console.log("CURRENT CATEGORY: ", currentCategory);
+    setNewsToggle(false);
   }
   React.useEffect(
     function () {
-      console.log("Use Effect");
       fetch(
-        "https://newsapi.org/v2/top-headlines?category=general&apiKey=bd662b9c617d48bca5aa4e291b6ea52f"
+        "https://newsapi.org/v2/top-headlines?category=" +
+          currentCategory +
+          "&language=en&apiKey=bd662b9c617d48bca5aa4e291b6ea52f"
       )
         .then((res) => res.json())
         .then((answer) => setNewsByCategory(answer.articles));
@@ -45,7 +56,8 @@ export default function App() {
     [currentCategory]
   );
 
-  let bodyOfNews = newsData.map((item) => {
+  let [newsToggle, setNewsToggle] = React.useState(false);
+  let bodyOfNews = (newsToggle ? newsData : newsByCategory).map((item) => {
     return (
       <NewsBody
         title={item.title}
@@ -57,15 +69,14 @@ export default function App() {
     );
   });
 
-  // console.log("This is the obtained DATA ", newsData[4].content);
-  // console.log("News Data Articles: ", newsData.articles[1]);
   return (
     <div className="App">
       <Header />
-      <CategoriesNavBar />
+      <CategoriesNavBar handleClick={changeCategory} />
       <button onClick={search} className="search">
         ClickToSearch
       </button>
+      {/* {newBodyOfNews} */}
       {bodyOfNews}
     </div>
   );
--- my-app/src/Components/CategoriesNavBar.js
+++ my-app/src/Components/CategoriesNavBar.js
@@ -1,16 +1,58 @@
 import React from "react";
 
-export default function CategoriesNavBar() {
+export default function CategoriesNavBar(props) {
   return (
     <div className="categoriesNavBar">
       <h2>Categories</h2>
-      <button id="general">General</button>
-      <button>Business</button>
-      <button>Entertainment</button>
-      <button>Health</button>
-      <button>Science</button>
-      <button>Sports</button>
-      <button>Technology</button>
+      <button
+        className="categoryButtons"
+        id="general"
+        onClick={props.handleClick}
+      >
+        General
+      </button>
+      <button
+        className="categoryButtons"
+        id="business"
+        onClick={props.handleClick}
+      >
+        Business
+      </button>
+      <button
+        className="categoryButtons"
+        id="entertainment"
+        onClick={props.handleClick}
+      >
+        Entertainment
+      </button>
+      <button
+        className="categoryButtons"
+        id="health"
+        onClick={props.handleClick}
+      >
+        Health
+      </button>
+      <button
+        className="categoryButtons"
+        id="science"
+        onClick={props.handleClick}
+      >
+        Science
+      </button>
+      <button
+        className="categoryButtons"
+        id="sports"
+        onClick={props.handleClick}
+      >
+        Sports
+      </button>
+      <button
+        className="categoryButtons"
+        id="technology"
+        onClick={props.handleClick}
+      >
+        Technology
+      </button>
     </div>
   );
 }
